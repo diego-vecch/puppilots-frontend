4fce33a042281248ec9e30c4dbe0ef5821901fdc
diego-vecch <josediegoferia@gmail.com>
2023-07-26 18:06:16 -0300

    feat: header home, desktop view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import FileCard from './components/Card'
 import { Grid, Col, Card, Text, Metric } from '@tremor/react'
+import Header from './components/Header'
 export default function Home (): JSX.Element {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex h-screen w-screen flex-col items-center justify-between bg-pup-container'>
+      <Header />
       <section className='rounded-md overflow-hidden w-full relative md:h-full'>
         <Grid numItems={1} numItemsSm={2} numItemsLg={3} className='gap-2'>
           <Col numColSpan={1} numColSpanLg={2}>
