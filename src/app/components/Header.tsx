e8a2c1b32540cbeafa8c264431bffd210a88778a
diego-vecch <josediegoferia@gmail.com>
2023-07-30 21:53:46 -0300

    fix: custom components header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,11 +5,11 @@ import { LogoApp } from './LogoApp'
 export default function Header (): JSX.Element {
   return (
     <header className='w-full flex justify-center'>
-      <nav className='text-pup-text_w_1 flex w-full justify-between px-1 py-3 max-w-[1400px]'>
+      <nav className='text-pup-text_w_1 flex w-full justify-center md:justify-between px-1 pt-5 pb-4 md:py-3 max-w-[1400px]'>
         <div className=''>
           <LogoApp />
         </div>
-        <div className=''>
+        <div className='hidden md:flex'>
           <ol className='flex [&>li]:px-2 py-1'>
             <li>
               <AncoreMenu>Inicio</AncoreMenu>
@@ -20,7 +20,7 @@ export default function Header (): JSX.Element {
             <li><AncoreMenu>Contacto</AncoreMenu></li>
           </ol>
         </div>
-        <div className='flex'>
+        <div className='hidden md:flex'>
           <div className='px-2 py-1'><ButtonWhite>Iniciar Sesión</ButtonWhite></div>
           <div className='px-2 py-1'><ButtonWhite>Registro</ButtonWhite></div>
         </div>
