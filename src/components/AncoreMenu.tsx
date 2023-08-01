079abc7889b3fb065e7d59e64c079c73ce172717
diego-vecch <josediegoferia@gmail.com>
2023-08-01 18:45:58 -0300

    ci: move components file to src file

---
+++ src/components/AncoreMenu.tsx
@@ -0,0 +1,12 @@
+type AuxProps = {
+  children: React.ReactNode
+}
+
+const AncoreMenu: React.FC<AuxProps> = ({ children }) => {
+  return (
+    <a className='w-full text-center bg-pup-menu hover:bg-slate-300 hover:text-pup-text_b_1 rounded-sm text-pup-text_w_1 px-3 py-1'>
+      {children}
+    </a>
+  )
+}
+export default AncoreMenu
---
+++ src/components/ButtonWhite.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+type AuxProps = {
+  children: React.ReactNode
+}
+const ButtonWhite: React.FC<AuxProps> = ({ children }) => {
+  return (
+    <button className='w-full bg-slate-200 hover:bg-slate-50 hover:text-purple-700 rounded-md text-pup-text_b_1 px-3'>
+      {children}
+    </button>
+  )
+}
+export default ButtonWhite
