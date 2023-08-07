e48833d2ab3da4ca9eaeaa83f542093dc63780b2
diego-vecch <josediegoferia@gmail.com>
2023-08-06 21:33:10 -0300

    fix: Modified Components Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,83 +1,15 @@
 'use client'
 
-import { Button } from '@tremor/react'
-import oneDog from '../../public/canwalk.webp'
-import twoDog from '../../public/cristian-castillo-73pyV0JJOmE-unsplash.jpg'
-import Image from 'next/image'
 import Menu from '../components/Menu'
 import Footer from '../components/Footer'
+import { HomeSection } from '@/components/HomeSection'
 
 export default function Home (): JSX.Element {
   return (
-    <main className='flex h-screen w-screen flex-col items-center justify-between bg-pup-container'>
+    <main className='flex h-full w-screen flex-col items-center justify-between bg-pup-container bg-gradient-to-t to-25% from-[#8f00ff] via-[#2d0c4d] via-15% to-pup-container'>
       <Menu />
-      <section className='rounded-md overflow-hidden w-3/4 relative md:h-full flex flex-col gap-20'>
-        <div className='flex flex-col justify-center items-center'>
-          <h1 className='text-pup-text_w_1 font-extrabold text-2xl tracking-wide'>Te ofrecemos nuestro servicio de paseos para perros</h1>
-          <h3 className='text-pup-text_w_1'>Paseamos a tu mascota </h3>
-        </div>
-        <div className='flex flex-row w-full justify-evenly'>
-          <div className='flex flex-col justify-center'>
-            <h1 className='text-pup-text_w_1 font-bold mb-3 mt-3'>Beneficio de nuestros clientes</h1>
-            <h3 className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</h3>
-            <h3 className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</h3>
-            <h3 className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</h3>
-            <h2 className='text-pup-text_w_1 font-bold mb-3 mt-3'>Deseas trabajar con nosotros?</h2>
-            <Button>Registro</Button>
-          </div>
-          <div>
-            <Image src={oneDog} alt='oneDog' width={300} height={300} />
-          </div>
-        </div>
-        <div className='flex flex-row w-full justify-evenly'>
-          <div>
-            <Image src={twoDog} alt='twoDog' width={300} height={300} />
-          </div>
-          <div className='flex flex-col justify-center'>
-            <h1 className='text-pup-text_w_1 font-bold mb-3 mt-3'>Beneficio de trabajar como paseador</h1>
-            <h3 className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</h3>
-            <h3 className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</h3>
-            <h3 className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</h3>
-            <h2 className='text-pup-text_w_1 font-bold mb-3 mt-3'>Deseas trabajar con nosotros?</h2>
-            <Button>Registro de Paseador</Button>
-          </div>
-        </div>
-
-        <div className='flex flex-col justify-center items-center'>
-          <h1 className='text-pup-text_w_1 font-extrabold text-2xl tracking-wide'>Pasos para coordinar un paseo para tu mascota</h1>
-
-          <div className='flex flex-row justify-evenly w-full'>
-            <div className='flex flex-col justify-center items-center'>
-              <div className='border rounded-2xl cursor-pointer border-stone-300 bg-gray-600 w-10 h-10 flex justify-center items-center text-2xl font-extrabold'>1</div>
-              <div><p className='text-pup-text_w_1 font-light'>✓ Registrate e inicia sesión</p></div>
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-              <div className='border rounded-2xl cursor-pointer border-stone-300 bg-gray-600 w-10 h-10 flex justify-center items-center text-2xl font-extrabold'>2</div>
-              <div><p className='text-pup-text_w_1 font-light max-w-xs'>✓  Seleccionar por día y horario a un paseador que se encuentre disponible.</p></div>
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-              <div className='border rounded-2xl cursor-pointer border-stone-300 bg-gray-600 w-10 h-10 flex justify-center items-center text-2xl font-extrabold'>3</div>
-              <div><p className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</p></div>
-            </div>
-
-          </div>
-
-        </div>
-
-        <div className='flex flex-row w-full justify-evenly'>
-          <div className='flex flex-col justify-center'>
-            <h1 className='text-pup-text_w_1 font-bold mb-3 mt-3'>Sobre Nosotros</h1>
-            <h3 className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</h3>
-            <h3 className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</h3>
-            <h3 className='text-pup-text_w_1 font-light'>✓ Manejo propio de horarios</h3>
-            <h2 className='text-pup-text_w_1 font-bold mb-3 mt-3'>Deseas trabajar con nosotros?</h2>
-            <Button>Registro</Button>
-          </div>
-          <div>
-            <Image src={oneDog} alt='oneDog' width={300} height={300} />
-          </div>
-        </div>
-      </section>
+      <HomeSection />
+      <div className=' w-full h-[26rem] fixed bottom-0 translate-y-8 bg-gradient-to-t from-[#570799] z-0 opacity-30 to-50%' />
       <Footer />
     </main>
   )
--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -2,7 +2,7 @@ import { SectionOfContent } from './SectionOfContent'
 import { SectionSteps } from './SectionSteps'
 export function HomeSection (): JSX.Element {
   return (
-    <section className='max-w-7xl overflow-hidden w-full relative md:h-full flex flex-col gap-20'>
+    <section className='max-w-7xl overflow-hidden w-full relative md:h-full flex flex-col gap-20 z-20'>
       <div className='px-2 flex flex-col py-20 md:py-60 font-lato text-xl justify-center items-center'>
         <div>
           <h1
@@ -25,6 +25,7 @@ export function HomeSection (): JSX.Element {
           item3='Seguimiento mediante la plataforma durante el paseo de tu perro'
           subTitle='Registrate para poder acceder a todos los servicios de nuestra plataforma'
           nameButton='Registro'
+          href='/register-client'
         />
       </div>
       <div className='px-4 pt-10 pb-14'>
@@ -33,11 +34,12 @@ export function HomeSection (): JSX.Element {
           src='/canwalk.webp'
           alt='Averiguar dueño de la img y de donde se obtuvo'
           titleSection='Beneficios de trabajar como paseador'
-          item1='Puedes acceder a un paseo para el día y hora que necesite tu mascota'
-          item2='Posibilidad de seleccionar al paseador'
-          item3='Seguimiento mediante la plataforma durante el paseo de tu perro'
+          item1='Puedes ofrecer tus servicios de acuerdo a tu disponibilidad'
+          item2='Posibilidad de elegir entre ofertas de paseo'
+          item3='Pago cada dos semanas'
           subTitle='Registrate para poder acceder a todos los servicios de nuestra plataforma'
           nameButton='Registro para paseadores'
+          href='/register-client'
         />
       </div>
       <div className='px-4 pt-4 pb-32'><SectionSteps /></div>
