66456cf55445c6148f667caf7ec833cb6ede2245
Ricardo Oyarzo Mansilla <raomaster@gmail.com>
2023-08-12 22:28:15 -0400

    Update page.tsx
    fixed problem with build

---
+++ src/app/home/client/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Menu from '../../../components/Menu'
+
+export default function HomePilot (): JSX.Element {
+  return (
+    <main className='bg-pup-container h-full w-screen justify-center place-items-center overflow-hidden'>
+      <div>
+        <Menu />
+      </div>
+    </main>
+  )
+}
