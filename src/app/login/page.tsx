d541c6cfa64b9d2b190b959ee9bec2b6fd57b12e
diego-vecch <josediegoferia@gmail.com>
2023-08-25 12:18:03 -0300

    fix: integration login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,38 +1,94 @@
 'use client'
-import Link from 'next/link'
-import { InputEmailForm, InputPasswordForm } from '../../components/InputForm'
+
+import { CustomInput } from '@/components/CustomInput'
 import { LabelForm } from '../../components/LabelForm'
 import { LogoApp } from '../../components/LogoApp'
-import ButtonWhite from '../../components/ButtonWhite'
+import { useState, useEffect } from 'react'
+import { useVerify } from '@/hooks/useVerify'
+import { JWT } from '@/types/userSession'
 
-export default function login (): JSX.Element {
+export default function Login (): JSX.Element {
+  const [token, setToken] = useState('')
+  const [registerOk, setRegisterOk] = useState(false)
+  const [loginUser, setLoginUser] = useState({
+    email: '',
+    password: ''
+  })
+  const [error, setError] = useState(false)
+  const [sendData, setSendData] = useState(false)
+  const send = (): void => {
+    setSendData(true)
+  }
+
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setLoginUser({
+      ...loginUser,
+      [e.target.name]: e.target.value
+    })
+  }
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+  }
+
+  useEffect(() => {
+    const linkUserLogin = process.env.NEXT_PUBLIC_USER_LOGIN as string
+    if (sendData) {
+      if (loginUser.email !== '' && loginUser.password !== '') {
+        const fetchRegister = async (): Promise<JWT> => {
+          return await fetch(`${linkUserLogin}`, {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json;charset=UTF-8'
+            },
+            body: JSON.stringify({
+              email: loginUser.email,
+              password: loginUser.password
+            })
+          }).then(async (res) => {
+            const data = await res.json()
+            if (res.ok) {
+              setRegisterOk(true)
+            }
+            return data
+          })
+        }
+        void fetchRegister().then((data) => {
+          setToken(data.access_token)
+        })
+      } else {
+        setError(true)
+      }
+    }
+  }, [sendData, loginUser])
+
+  useVerify(registerOk, token)
+
   return (
     <div className='bg-pup-container h-screen flex justify-center place-items-center'>
-      <div className='absolute inset-x-0 top-4 w-full pt-4 flex justify-center'><LogoApp /></div>
-      <div className='w-[400px] h-[300px] max-w-5xl max-h-[670px] flex border-indigo-700 border-opacity-60 rounded-2xl border-2'>
-        <div className='h-full relative rounded-l-2xl'>
-          <div
-            className='rounded-l-2xl absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-indigo-900 bg-gradient-to-b from-pup-container from-10% via-indigo-950 via-15%% to-indigo-900 to-95%% bg-fixed opacity-30'
-          />
-        </div>
-        <form className='w-full h-full px-2 xs:px-6 sm:px-8 md:px-16 pt-6 pb-8 flex flex-col [&>p]:flex [&>p]:flex-col [&>p]:h-full [&>p>label]:pb-1 [&>p>label]:text-pup-text_w_2'>
-          <div>
-            <p className='text-center flex place-content-center text-xl text-pup-text_w_2 pt-1 pb-8 font-lato bold'>Iniciar Sesión</p>
-          </div>
-          <p>
-            <LabelForm>Email</LabelForm>
-            <InputEmailForm />
-          </p>
-          <p>
-            <LabelForm>Contraseña</LabelForm>
-            <InputPasswordForm />
-          </p>
-          <div className='flex justify-center'>
-            <div className='text-right pt-2 pb-4 sm:1/2 md:w-1/4'>
-              <ButtonWhite><Link href='./'>Enviar</Link></ButtonWhite>
+      <div className='w-[400px] h-[380px] max-w-5xl max-h-[670px] flex border-indigo-700 border-opacity-60 rounded-2xl border-2'>
+        <div className='w-full h-full grid'>
+          <div className='h-[50px] w-full flex items-center justify-center mt-5'><LogoApp /></div>
+          <form onSubmit={handleSubmit} className='w-full h-full px-2 xs:px-4 sm:px-6 md:px-8 pt-1 pb-3 flex flex-col [&>p]:flex [&>p]:flex-col [&>p]:h-full [&>p>label]:pb-1 [&>p>label]:text-pup-text_w_2'>
+            <div>
+              <p className='text-center flex place-content-center text-xl text-pup-text_w_2 pt-1 pb-6 font-lato bold'>Iniciar Sesión</p>
             </div>
-          </div>
-        </form>
+            <section className='[&div]:w-full h-[85px]'>
+              <div><LabelForm>Email</LabelForm></div>
+              <div><CustomInput onChange={inputChange} value={loginUser.email} name='email' type='email' placeholder='ejemplo: juan-juan@gmail.com' /></div>
+              {error && (<p className='text-red-400'>&nbsp;Campo Obligatorio</p>)}
+            </section>
+            <section className='[&div]:w-full h-[85px]'>
+              <div><LabelForm>Contraseña</LabelForm></div>
+              <div><CustomInput onChange={inputChange} value={loginUser.password} name='password' type='password' placeholder='ejemplo: 123@Aabcdf#' /></div>
+              {error && (<p className='text-red-400'>&nbsp;Campo Obligatorio</p>)}
+            </section>
+            <section className='flex justify-center'>
+              <div className='text-center pt-2 pb-4 sm:1/2 md:w-1/4'>
+                <button onClick={send} type='submit'>Enviar</button>
+              </div>
+            </section>
+          </form>
+        </div>
       </div>
     </div>
   )
