6a305688583c84b9fb543a913d38cd683860338f
diego-vecch <josediegoferia@gmail.com>
2023-08-25 17:31:15 -0300

    fix: update info user context with useVerify

--- src/hooks/useVerify.tsx
+++ src/hooks/useVerify.tsx
@@ -2,14 +2,18 @@ import { useState, useEffect, useContext } from 'react'
 import { ContextUser } from '@/context/ContextUser'
 import { verifyTokenUser } from '../services/verifyTokenUser'
 import { redirect } from 'next/navigation'
+import { ContextUserInfo } from '@/context/ContexUserInfo'
 
 export const useVerify = (registerOk: boolean, token: string): null => {
+  const { setInfoOfUser } = useContext(ContextUserInfo)
   const { setInfoUser } = useContext(ContextUser)
   const [rolUser, setRol] = useState('')
   useEffect(() => {
     const linkUserVerifyToken = process.env.NEXT_PUBLIC_USER_VERIFY as string
     if (registerOk) {
       void verifyTokenUser(token, linkUserVerifyToken).then(res => {
+        sessionStorage.setItem('info', `${JSON.stringify(res)}`)
+        setInfoOfUser('' ?? sessionStorage.getItem('info'))
         setRol(res.role.toLowerCase())
         sessionStorage.setItem('email', res.email)
         sessionStorage.setItem('isLogged', 'true')
@@ -26,6 +30,6 @@ export const useVerify = (registerOk: boolean, token: string): null => {
         redirect('/home/pilot')
       }
     }
-  }, [registerOk, token, setInfoUser, rolUser])
+  }, [registerOk, token, setInfoUser, rolUser, setInfoOfUser])
   return null
 }
