c075c926bb09876abd2390681236f361bed0962e
diego-vecch <josediegoferia@gmail.com>
2023-08-26 09:16:30 -0300

    fix: upload state of ContextUserInfo

--- src/context/ContexUserInfo.tsx
+++ src/context/ContexUserInfo.tsx
@@ -13,8 +13,9 @@ export const ContextUserInfo = createContext({
 })
 
 export const ProviderInfoOfUser: React.FC<PropsContext> = ({ children }) => {
-  const info = window.sessionStorage.getItem('info') ?? '{}'
-  const infoObject = JSON.parse(info)
+  const infoSessionStorage = typeof window !== 'undefined' && window.sessionStorage.getItem('info')
+  const info = infoSessionStorage ?? '{}'
+  const infoObject = JSON.parse(info as string)
   const [infoOfUser, setInfoOfUser] = useState<Data>(infoObject)
   useEffect(() => { console.log(infoObject) }, [infoObject])
   return (<ContextUserInfo.Provider value={{ infoOfUser, setInfoOfUser }}>{children}</ContextUserInfo.Provider>)
